350fadd47ba517fae366142e078e37f05cd698e5
bob-ROS <fredrik.ano@gmail.com>
2019-11-06 10:35:47 +0100

    albert added lots of stuff

---
+++ asc_autorun/launch/cp.sh
@@ -0,0 +1,1 @@
+sshpass -p 123 scp run_lidar_free_real.launch abbe@ABB:~/catkin_ws/src/asc_project/asc_autorun/launch
---
+++ asc_republisher/src/asc_cmdvel.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include "ros/ros.h"
+//#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
+#include <memory>
+
+ros::Publisher chatter_pub;
+
+void cmdCallback(const geometry_msgs::Twist::ConstPtr & msg)
+{
+    ros::NodeHandle n;
+
+    chatter_pub.publish(msg);
+}
+
+
+int main(int argc, char** argv){
+
+  ros::init(argc, argv, "republisher");
+  ros::NodeHandle n;
+  chatter_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+  ros::Subscriber sub = n.subscribe("/cmd_vel_xbox", 1000, cmdCallback);
+  ros::spin();
+
+
+  return 0;
+}
