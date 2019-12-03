12d0d98b183c1c29a204b91c50d70c51fad6d1c0
bob-ROS <fredrik.ano@gmail.com>
2019-12-03 13:20:11 +0100

    fixed local costmap

--- asc_navigation/scripts/flex_smach.py
+++ asc_navigation/scripts/flex_smach.py
@@ -57,7 +57,7 @@ def main():
             ),
             transitions={
                 'succeeded': 'EXE_PATH',
-                'aborted': 'WAIT_FOR_GOAL',
+                'aborted': 'EXE_PATH',
                 'preempted': 'preempted'
             },
             remapping={
